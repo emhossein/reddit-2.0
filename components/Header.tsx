b022590a5c0ea7137d073efc8453952b40ffbfd0
Mohammad <mhossein796@gmail.com>
2022-05-25 17:25:50 +0430

    icons fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,30 @@
 import Image from 'next/image';
 import React from 'react';
-import { BellIcon } from '@heroIcons/react/outline';
-import { ChatIcon } from '@heroIcons/react/outline';
-import { GlobeIcon } from '@heroIcons/react/outline';
-import { PlusIcon } from '@heroIcons/react/outline';
-import { SparklesIcon } from '@heroIcons/react/outline';
-import { SpeakerphoneIcon } from '@heroIcons/react/outline';
-import { VideoCameraIcon } from '@heroIcons/react/outline';
+// import { BellIcon } from '@heroIcons/react/outline';
+// import { ChatIcon } from '@heroIcons/react/outline';
+// import { GlobeIcon } from '@heroIcons/react/outline';
+// import { PlusIcon } from '@heroIcons/react/outline';
+// import { SparklesIcon } from '@heroIcons/react/outline';
+// import { SpeakerphoneIcon } from '@heroIcons/react/outline';
+// import { VideoCameraIcon } from '@heroIcons/react/outline';
 
-import { ChevronDownIcon } from '@heroIcons/react/solid';
-import { HomeIcon } from '@heroIcons/react/solid';
-import { MenuIcon } from '@heroIcons/react/solid';
-import { SearchIcon } from '@heroIcons/react/solid';
+// import { ChevronDownIcon } from '@heroIcons/react/solid';
+// import { HomeIcon } from '@heroIcons/react/solid';
+// import { MenuIcon } from '@heroIcons/react/solid';
+// import { SearchIcon } from '@heroIcons/react/solid';
+import {
+  BellOutline,
+  ChatOutline,
+  GlobeOutline,
+  PlusOutline,
+  SparklesOutline,
+  SpeakerphoneOutline,
+  VideoCameraOutline,
+  ChevronDownSolid,
+  HomeSolid,
+  MenuSolid,
+  SearchSolid
+} from '@graywolfai/react-heroicons';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 
@@ -26,12 +39,12 @@ function Header() {
         </Link>
       </div>
       <div className="mx-7 flex items-center xl:min-w-[300px]">
-        <HomeIcon className="h-5 w-5" />
+        <HomeSolid className="h-5 w-5" />
         <p className="ml-2 hidden flex-1 lg:inline">Home</p>
-        <ChevronDownIcon className="h-5 w-5" />
+        <ChevronDownSolid className="h-5 w-5" />
       </div>
       <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 bg-gray-100 px-3 py-1">
-        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <SearchSolid className="h-6 w-6 text-gray-400" />
         <input
           className="flex-1 bg-transparent outline-none"
           type="text"
@@ -40,17 +53,17 @@ function Header() {
         <button hidden type="submit" />
       </form>
       <div className="mx-5 hidden items-center space-x-2 text-gray-500 lg:inline-flex ">
-        <SparklesIcon className="icon" />
-        <GlobeIcon className="icon" />
-        <VideoCameraIcon className="icon" />
+        <SparklesOutline className="icon" />
+        <GlobeOutline className="icon" />
+        <VideoCameraOutline className="icon" />
         <hr className="h-10 border border-gray-100" />
-        <ChatIcon className="icon" />
-        <BellIcon className="icon" />
-        <PlusIcon className="icon" />
-        <SpeakerphoneIcon className="icon" />
+        <ChatOutline className="icon" />
+        <BellOutline className="icon" />
+        <PlusOutline className="icon" />
+        <SpeakerphoneOutline className="icon" />
       </div>
       <div className="ml-5 flex items-center lg:hidden">
-        <MenuIcon className="icon" />
+        <MenuSolid className="icon" />
       </div>
       {session ? (
         <div
@@ -63,7 +76,7 @@ function Header() {
             <p className="truncate">{session?.user?.name}</p>
             <p className="text-gray-400 ">1 Karma</p>
           </div>
-          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
+          <ChevronDownSolid className="h-5 flex-shrink-0 text-gray-400" />
         </div>
       ) : (
         <div
